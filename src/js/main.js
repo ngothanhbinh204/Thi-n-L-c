223b3aa7f2eac4a37ed1723ac0a970ff8d9aaec8
ngothanhbinh <ntbinh14122004@gmail.com>
2025-12-09 09:14:08 +0700

    first Commit

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+import AOS from "aos";
+import lozad from "lozad";
+import { setBackgroundElement, buttonToTop, menuSpy, stickElementToEdge } from "./helper";
+import { header } from "./header";
+import { swiperInit } from "./swiper";
+$(document).ready(function () {
+	setBackgroundElement();
+	stickElementToEdge();
+	menuSpy();
+	buttonToTop();
+	header.init();
+	swiperInit();
+});
+
+/*==================== Aos Init ====================*/
+AOS.init({
+	offset: 100,
+});
+/*==================== Lazyload JS ====================*/
+const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+observer.observe();
+
+window.FE = {
+	lozad: observer.observe,
+}
